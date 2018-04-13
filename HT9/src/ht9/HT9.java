dd691a81d98873952d27606e21433f8ee322c985
DESKTOP-1F2LFR5\SDiego <sev17238@uvg.edu.gt>
2018-04-13 11:30:24 -0600

    first commit

---
+++ HT9/src/ht9/HT9.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ht9;
+
+/**
+ *
+ * @author David Soto 17555 - David Soto 17238
+ */
+public class HT9 {
+    int los314ing2esp2;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
